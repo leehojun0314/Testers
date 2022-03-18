0129c501677edb5b1c5aba4593a93a36fcacfdc6
leehojun0314 <dlghwns0314@naver.com>
2022-03-18 22:06:28 +0900

    bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Header from "../components/Header";
+import Header from "../components/common/Header";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
 import GlobalStyle from "../styles/Global";
